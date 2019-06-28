348963f52573641273a437c6249775844f7407a9
Marcelo M. S. Souza <marcelo@ufc.br>
2019-06-28 13:37:25 -0300

    Added parameters file parsing

--- ant.py
+++ ant.py
@@ -8,66 +8,62 @@ Created on Mon Jul  2 20:10:52 2018
 
 import scipy
 import pylab
-
+import configparser
 from scipy.spatial.distance import pdist,squareform
 from multiprocessing import Pool
+from functools import partial
 
 raw_data = pylab.loadtxt("a280.tsp",skiprows = 6,usecols = (1,2),comments='E')
-data = squareform(pdist(raw_data))
+dist = squareform(pdist(raw_data))
+n = dist.shape[0]
+for i in range(n):
+ dist[i,i] = 1.
 
-nf = data.shape[0]
+config = configparser.ConfigParser()
+config.read("parameters.conf")
+parms = config['Default']
 
-for i in range(nf):
- data[i,i] = 1.
+#n_hf= 5 # hall of fame (top n_hf best solutions so far)
+na = int(parms['na']) # ants number
+alpha = float(parms['alpha']) # ant trend to follow collective trails
+beta = float(parms['beta']) # ant trend to follow individual shortest path
+rho = float(parms['rho']) # pheromone evaporation rate
+Q = float(parms['Q']) # pheromone deposit gain
+Epsilon = float(parms['Epsilon']) # smallest pheromone deposit
 
-#n_hf = 5 # hall of fame (top n_hf best solutions so far)
-na = 75 # ants number
-alpha = 1.4 # ant trend to follow collective trails
-beta = 1.4 # features independence influence
-rho = 0.3 # pheromone evaporation rate
-Q = 250.  # pheromone deposit gain
-# fitness matrix
-tau = scipy.rand(nf+1,nf) # pheromone matrix
-fit = scipy.rand(na)
 
-def fitness_func(x):
-  return pylab.sum([data[i,j] for i,j in zip(x,x[1:])])
-
-def Sorteia(k,tau,nu = None):
-   if nu is None:
-      nu = pylab.ones(nf)
-   p = scipy.array([tau[k,j]**alpha * (1./nu[j])**beta for j in scipy.arange(nf)])
-   p = (scipy.hstack(([0],p))/p.sum()).cumsum()
+def Sorteia(k,tau,nu = pylab.ones((n,n))):
+   p = scipy.array([(tau[k,j]**alpha) * (1./nu[k,j]**beta) for j in range(n)]) 
+   p = scipy.hstack(([0],p/p.sum())).cumsum()
    return scipy.where(scipy.rand() > p)[0].argmax()
 
-def GeraSolucoes(i):
- # Busca o primeiro atributo
-   aux = Sorteia(0,tau)
-   U = list(range(nf))
-   # Busca dos n-1 demais atributos e coloca na lista l
-
-   l = []
-   while len(l) < nf:
+def GeraSolucoes(i,tau):
+   U = list(range(n))
+   orig = 0
+   l = [orig]
+   U.remove(orig)
+   
+   while len(l) < n:
+     aux = Sorteia(orig,tau,dist)
      if aux in U:
       l.append(aux)
       U.remove(aux)
-     #print(data[aux])
-     aux = Sorteia(aux,tau[1:,:],data[aux])
+      orig = aux
    return l
 
 def AvaliaSolucoes(s):
-   fit = scipy.array([fitness_func(x) for x in s])
-   return fit
+   f = lambda x: pylab.sum([dist[i,j] for i,j in zip(x,x[1:])])
+   return pylab.array([f(x) for x in s])
 
-def AtualizaFeromonios(s,tau):
+def AtualizaFeromonios(s,tau,fit):
+
    for (i,xa) in enumerate(s):
-    k = xa[0]
-    tau[0,k] =  tau[0,k] + Q/fit[i]
-    tau_s = tau[1:,:]
-    for j in xa[1:]:
-     tau_s[k,j] =  tau_s[k,j] + Q/fit[i]
-     tau_s[j,k] =  tau_s[j,k] + Q/fit[i]
-     k = j
+    for j,k in zip(xa[0:],xa[1:]):
+     tau[j,k] =  tau[j,k] + Q/float(fit[i])
+     tau[k,j] =  tau[k,j] + Q/float(fit[i])
+   idx =  pylab.where(tau>Epsilon)
+   tau[idx] = (1.-rho)*tau[idx]
+    
    return tau
 
 #def HF_Updt(hf,x):
@@ -85,12 +81,12 @@ def AtualizaFeromonios(s,tau):
 
 if __name__ == '__main__':
 # hf = []
- for kk in scipy.arange(2000):
-  #tt = process_time()
-  sa= Pool().map(GeraSolucoes,range(na))
+ p = Pool(4)
+ tau = Epsilon*pylab.ones((n,n))
+ for kk in scipy.arange(500):
+  sa = p.map(partial(GeraSolucoes, tau=tau),range(na))
   fit = AvaliaSolucoes(sa)
-  tau = AtualizaFeromonios(sa,tau)
-  tau = (1. - rho) * tau
+  tau = AtualizaFeromonios(sa,tau,fit)
   id_max = fit.argmax()
   id_min = fit.argmin()
   print("{:4d} {:5.0f} {:5.0f} {:5.0f} {:5.2f}".format(kk,fit[id_max],fit[id_min],fit.mean(),fit.std()))
