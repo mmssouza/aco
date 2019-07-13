3e378f25a2d7b03b63c06596ca6b3d1615f36afc
Marcelo M. S. Souza <marcelo@ufc.br>
2019-07-13 04:54:26 -0300

    sei la

--- ant.py
+++ ant.py
@@ -8,108 +8,85 @@ Created on Mon Jul  2 20:10:52 2018
 
 import scipy
 import pylab
-#from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import scale
 #from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import StratifiedKFold,cross_val_score
 from sklearn.metrics import mutual_info_score
 from sklearn.neighbors import KNeighborsClassifier
-#from sklearn.naive_bayes import GaussianNB
-from numpy.random import permutation
-from time import process_time
-
+import pickle
 
 def calc_MI(x, y, bins):
-    c_xy = scipy.histogram2d(x, y, bins)[0]
+    c_xy = scipy.histogram2d(x, y, bins, normed = False)[0]
     mi = mutual_info_score(None, None, contingency=c_xy)
     return mi
 
-x = pylab.loadtxt("madelon_train.data")
-l = pylab.loadtxt("madelon_train.labels")
+x = pickle.load(open("kimia99.data","rb"))
+l = pickle.load(open("kimia99.labels","rb"))
+x = pylab.log(x)[:,0:99:5]
 
 ns,nf = x.shape
 
-#idx1 = permutation(scipy.arange(6000))[0:1000]
-#idx2 = permutation(scipy.arange(5000))[0:2500]
-#x = x[idx1]
-#x = x[:,idx2]
-#l = l[idx1]
-idx1 = permutation(scipy.arange(ns))[0:1000]
-idx2 = permutation(scipy.arange(nf))[0:50]
-x = (x[idx1])[:,idx2]
-l = l[idx1]
-nf = x.shape[1]
-print(x.shape)
+mi = 1. - scipy.zeros((nf,nf))
 
-mi = scipy.zeros((nf,nf))
 for i in range(nf):
  for j in range(nf):
-   mi[i,j] = calc_MI(x[:,i],x[:,j],10)
+   mi[i,j] = calc_MI(x[:,i],l,5)
 
 #n_hf = 5 # hall of fame (top n_hf best solutions so far)
-na = 150 # ants number
-alpha = 1.5 # ant trend to follow collective trails
-beta = 0. # features independence influence
+na = 20#nts number
+alpha = .9# ant trend to follow collective trails
+#beta =  0. # features independence influence
 #gamma = .5  # accurancy vs dimensionality of solution tradeoff
-rho = 0.15 # pheromone evaporation rate
-Q = 10.  # pheromone deposit gain
+rho = 0.15# pheromone evaporation rate
+Q = 1.5#pheromone deposit gain
+Epsilon = 0.01
+
 # fitness matrix
-tau = Q*scipy.ones((nf+2,nf)) # pheromone matrix
+tau = Epsilon*scipy.ones((nf,nf)) # pheromone matrix
 fit = scipy.array([[scipy.rand(),0] for i in range(na)])
 
 def fitness_func(features,xx,yy):
  #clf = GaussianNB()
  clf = KNeighborsClassifier(n_neighbors = 3,algorithm = 'kd_tree')
- score = cross_val_score(clf,xx[:,features],cv = StratifiedKFold(n_splits = 10),y = yy,scoring='roc_auc')
+ score = cross_val_score(clf,xx[:,features],cv = StratifiedKFold(n_splits = 3),y = yy,scoring='accuracy')
  m,s = score.mean(),score.std()
 # print(features.shape,ss)
  return [m,s]
 
 
-def Sorteia(k,tau,nu = pylab.ones(nf)): 
-   p = scipy.array([tau[k,j]**alpha * nu[j]**beta for j in scipy.arange(nf)])
+def walk(orig,tau,nu = pylab.ones((nf,nf))): 
+   p = scipy.array([(tau[orig,j]**alpha) * (nu[orig,j]**(1.-alpha)) for j in scipy.arange(nf)])
    p = (scipy.hstack(([0],p))/p.sum()).cumsum()
-   return scipy.where(scipy.rand() > p)[0].argmax()
+   dest = scipy.where(scipy.rand() > p)[0].argmax()
+   return dest
 
-#def Sorteia_n(k,tau): 
-#   p = scipy.array([tau[k,j]**alpha for j in scipy.arange(nf)])
-#   p = (scipy.hstack(([0],p))/p.sum()).cumsum()
-#   return scipy.where(scipy.rand() > p)[0].argmax()
 
-#def Sorteia_first(t):
-#   p = scipy.array([t[1,j]**alpha for j in scipy.arange(nf)])
-#   p = (scipy.hstack(([0],p))/p.sum()).cumsum()
-#   return scipy.where(scipy.rand() > p)[0].argmax()
-
-#def Sorteia_others(k,t):
-#   p = scipy.array([(t[k,j]**alpha)*(1./(1e-3 + mi[k,j])**beta) for j in scipy.arange(nf)])
-#   p = (scipy.hstack(([0],p))/p.sum()).cumsum()
-#   return scipy.where(scipy.rand() > p)[0].argmax()
-
-def GeraSolucoes():
+def GeraSolucoes(nest,nattr):
 
   s = []
   # sorteia origem como funcao dos feromonios (orig)
   for i in scipy.arange(na):
    # sorteia número de atributos
-   n = Sorteia(0,tau) + 1
+   #nattr = walk(0,tau) + 1
+#   print(n,end=" ")
+   l = [nest]
    # Busca o primeiro atributo
-   aux = Sorteia(1,tau)
+   orig = nest
+#   print(aux,end=" "
+   #print(i,end = ' ')
+   #print(nattr,end = ' ')
+   #print(orig,end = ' ')
    # Busca dos n-1 demais atributos e coloca na lista l
-   l = []
-   for k in scipy.arange(n-1):
-    l.append(aux)
-    aux = Sorteia(aux,tau[2:,:],1./mi[aux,:])
-   # elimina repetidos e identifica os atributos não pertences a l
-   s1 = set(l)
-   l = list(s1)
-   u = list(set(range(nf)) - s1)
-   # Adiciona aleatóriamente, no lugar dos atributos repetidos
-   # aqueles que não pertencem a l até que a cardinalidade
-   # atinja n
-   while (len(l) < n):
-     u = list(permutation(u))
-     l.append(u.pop())
-   s.append(scipy.sort(l))
+   while len(l) < nattr:
+    dest = walk(orig,tau,1/(mi+1))
+    if dest not in l:
+     l.append(dest)
+     #print(dest,end = ' ')
+     orig = dest
+   #print() 
+#   print(pylab.array(l)
+   s.append(pylab.sort(l))
+   
   return s
 
 def AvaliaSolucoes(s,x,l):
@@ -117,44 +94,40 @@ def AvaliaSolucoes(s,x,l):
    fit = scipy.array([fitness_func(xa,x,l) for xa in s])
    return(fit)
 
-def AtualizaFeromonios(s,tau):
-
-   for (i,xa) in enumerate(s):
-    sz = xa.shape[0]
-    gain_fit = fit[i,0]    # pheromone ant gains due to classification accurancy
-    #gain_length = (1./((float(sz)/float(nf))+1.)**2)  # pheromone ant gains due to number of features
-    #gain = Q*(gain_fit**gamma)*(gain_length**(1-gamma))  # accurancy vs number of features tradeoff
-    gain = Q*gain_fit  # accurancy vs number of features tradeoff
-    tau[0,sz-1] = tau[0,sz-1] + gain
-    tau_s = tau[1:,:]
-    k = xa[0]
-    for j in xa[1:]:
-     tau_s[k,j] =  tau_s[k,j] + gain
-     k = j
-   tau =  (1. - rho)*tau
-
-def HF_Updt(hf,x):
-  if len(hf) < n_hf:
-   hf.append(x)
-   hf.sort(reverse = True)
-   return
+def AtualizaFeromonios(s): 
+   
+   ix = pylab.where(tau > Epsilon)
+   tau[ix] = (1. - rho)*tau[ix] 
+    
+   for (i,xa) in enumerate(s):   
+    for j,k in zip(xa[0:],xa[1:]):   
+     tau[j,k] =  tau[j,k] + Q*fit[i,0]
+    #tau[k,j] =  tau[k,j] + Q*fit[i,0]
+  
+#def HF_Updt(hf,x):
+#  if len(hf) < n_hf:
+#   hf.append(x)
+#   hf.sort(reverse = True)
+#   return
 
   # Hall of fame
-  aux = scipy.where(x[0] > scipy.array([i[0] for i in hf]))
-  if len(aux[0]) != 0:
-   hf.insert(aux[0].min(),x)
-   hf.pop()
+#  aux = scipy.where(x[0] > scipy.array([i[0] for i in hf]))
+#  if len(aux[0]) != 0:
+#   hf.insert(aux[0].min(),x)
+#   hf.pop()
 
 if __name__ == '__main__':
- hf = []
+# hf = []
+ nest = 5
+ nattr = 10
  for kk in scipy.arange(3000):
-  tt = process_time()
-  sa =  GeraSolucoes()
+  sa =  GeraSolucoes(nest,nattr)
   fit = AvaliaSolucoes(sa,x,l)
-  AtualizaFeromonios(sa,tau)
+  AtualizaFeromonios(sa)
   id_max = fit[:,0].argmax()
   id_min = fit[:,0].argmin()
-  print(kk,process_time() - tt,len(sa[id_max]),fit[id_max],fit[id_min],fit[:,0].mean())
-  print(sa[id_max])
+  #print(kk,len(sa[id_max]),fit[id_max],fit[id_min],fit[:,0].mean())
+  #print(pylab.sort(sa[id_max]))
+  print(kk,fit[id_max][0],fit[id_min][0],fit[:,0].mean())
   #HF_Updt(hf,[fit[id_max][0],sa[id_max]])
  #print(hf)
